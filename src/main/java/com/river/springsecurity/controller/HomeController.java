564391cab4852a8fe9d1889bbd0248a81b76ac7e
ttgiang275 <ttgiang275@gmail.com>
2025-02-17 21:51:50 +0700

    Init commit

---
+++ src/main/java/com/river/springsecurity/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.river.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "home";
+    }
+}
